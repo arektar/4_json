944f84e5b7f9d8dca26ab8c85e403d3eb38d729e
arektar <zander5@mail.ru>
2017-01-16 15:35:19 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,20 @@
 import json
 
-def load_data(filepath):
-    with open(filepath) as json_file:
+
+def load_data(file_path):
+    with open(file_path) as json_file:
         bars_data = json_file.read()
     return json.loads(bars_data)
 
 
 def pretty_print_json(data):
-    to_print=json.dumps(data, indent=4, sort_keys=True)
+    to_print = json.dumps(data, indent=4, sort_keys=True)
     return to_print
 
 
 if __name__ == '__main__':
-    filepath=input("Введите полное имя файла .json: ")
-    json_data = load_data(filepath)
-    to_show_data=pretty_print_json(json_data)
+    # my_file_path=input("Введите полное имя файла .json: ")
+    my_file_path = 'data-2897-2016-11-23\data-2897-2016-11-23.json'
+    json_data = load_data(my_file_path)
+    to_show_data = pretty_print_json(json_data)
     print(to_show_data)
